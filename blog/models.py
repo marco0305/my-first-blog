573b85535debdc3fffc405804f1fa76b09b4dc24
Marco0305 <smallflower0305@gmail.com>
2017-06-09 11:59:12 +0800

    git commit

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 from django.utils import timezone
 
+#my first blog models.
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length = 200)
