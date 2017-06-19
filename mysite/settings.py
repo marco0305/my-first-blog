c2b3933cc6b3e1475e0132cba20b71a3e014b109
Marco0305 <smallflower0305@gmail.com>
2017-06-19 16:50:15 +0800

    OK

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['marco0305.pythonanywhere.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 
-
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^polls/', include('polls.urls'))
 ]
